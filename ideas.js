dd809379c84dcc54832bac7bceb84bfb289cb8b5
ironnerves <60079947+ironnerves@users.noreply.github.com>
2020-02-18 15:52:15 -0500

    Update ideas.js

--- ideas.js
+++ ideas.js
@@ -3,4 +3,5 @@ This is where ideas for the website go and when we need to, we use them
 */
 /*
 Idea 1: Add more comments to make the code more readable
+Idea 2: Have more code so that we could have more stuff to brag about when we are getting a job.
 */
